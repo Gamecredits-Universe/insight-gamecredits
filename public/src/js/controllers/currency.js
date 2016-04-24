c5267c4f17c908f8db48cf37e72cb5d544f77750
GameCredits <asksecinfo@gmail.com>
2016-04-24 12:05:31 +0200

    up

--- public/src/js/controllers/currency.js
+++ public/src/js/controllers/currency.js
@@ -20,6 +20,7 @@ angular.module('insight.currency').controller('CurrencyController',
 
         if (this.symbol === 'USD') {
           response = _roundFloat((value * this.factor), 2);
+        } else if (this.symbol === 'mGAME') {
           this.factor = 1000;
           response = _roundFloat((value * this.factor), 5);
         } else if (this.symbol === 'bits') {
@@ -46,6 +47,7 @@ angular.module('insight.currency').controller('CurrencyController',
         Currency.get({}, function(res) {
           $rootScope.currency.factor = $rootScope.currency.bitstamp = res.data.bitstamp;
         });
+      } else if (currency === 'mGAME') {
         $rootScope.currency.factor = 1000;
       } else if (currency === 'bits') {
         $rootScope.currency.factor = 1000000;
